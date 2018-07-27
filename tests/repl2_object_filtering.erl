3cbf7b1ac54a86e5497b3a5636b05b80a18c7b6d
Nordine Saadouni <saadouni85@gmail.com>
2018-07-27 16:30:50 +0100

    new object filtering test, error seems to be not deploying 8 nodes

--- tests/repl2_object_filtering.erl
+++ tests/repl2_object_filtering.erl
@@ -5,77 +5,93 @@
 
 
 confirm() ->
-    %% run fullsync tests
     [ run_fullsync_test(N) || N <- lists:seq(1,1)],
-
-    %% run realtime tests
-%%    [ run_realtime_test(N) || N <- lists(1,1)],
     pass.
 
 run_fullsync_test(N) ->
     print_test(fullsync, N),
     ?assertEqual(pass, fullsync_test(N)).
 
-%%run_realtime_test(N) ->
-%%    print_test(realtime, N),
-%%    ?assertEqual(pass, realtime_test(N)).
-
-
-print_test(TestType, N) ->
+print_test(Name, Number) ->
     lager:info("---------------------------------------"),
     lager:info("---------------------------------------"),
     lager:info("---------------------------------------"),
-    lager:info("-------- Test ~p  ~p ---------------", [TestType, N]),
+    lager:info("-------- Test ~p ~p ---------------", [Name, Number]),
     lager:info("---------------------------------------"),
     lager:info("---------------------------------------"),
     lager:info("---------------------------------------").
 
-
-
 %% ================================================================================================================== %%
-%%                                         Fullsync Tests                                                             %%
+%%                                        Fullsync Tests                                                              %%
 %% ================================================================================================================== %%
-
 fullsync_test(1) ->
-    make_cluster(2,5),
+    Nodes = make_clusters(2, 5),
+    [_Cluster1, _Cluster2] = Nodes,
     pass.
-
 %% ================================================================================================================== %%
-%%                                         Helper Fucntions                                                           %%
+%%                                        Riak Test Functions                                                         %%
 %% ================================================================================================================== %%
 
-make_cluster(N, ClusterSize) ->
-    NumberOfNodes = N*ClusterSize,
-    lager:info("Deploy ~p nodes", [NumberOfNodes]),
+make_clusters(NumberOfClusters, NumberOfNodesPerCluster) ->
+
+    TotalNumberOfNodes = NumberOfClusters * NumberOfNodesPerCluster,
+    lager:info("Deploy ~p nodes", [TotalNumberOfNodes]),
+
     Conf = [
-        {riak_core,
+        {riak_repl,
             [
-                {ring_creation_size, 64},
-                {default_bucket_props, [{n_val, 3}, {w,3}, {dw,3}, {r,3}, {allow_mult, false}]}
-            ]
-        },
-
+                %% turn off fullsync
+                {fullsync_on_connect, false},
+                {fullsync_interval, disabled}
+            ]},
         {riak_kv,
             [
                 {backend_reap_threshold, 86400}
-            ]
-        }
+            ]}
     ],
-    rt:deploy_nodes(NumberOfNodes, Conf, [riak_kv, riak_repl]).
-%%    [make_cluster_helper(Nodes, ClusterNumber, ClusterSize) || ClusterNumber <- lists:seq(1,N)].
 
-%%make_cluster_helper(Nodes, ClusterNumber, ClusterSize) ->
-%%    N = ((ClusterNumber*ClusterSize) +1) -5,
-%%    ClusterNodes = lists:sublist(Nodes, N, ClusterSize),
-%%    lager:info("N ~p -> Cluster Nodes ~p", [N, ClusterNodes]),
-%%    repl_util:make_cluster(ClusterNodes),
-%%    lager:info("waiting for leader to converge on cluster ~p", [ClusterNumber]),
-%%    ?assertEqual(ok, repl_util:wait_until_leader_converge(lists:nth(1, ClusterNodes))),
-%%    ClusterNodes.
+    Nodes1 = rt:deploy_nodes(8, Conf, [riak_kv, riak_repl]),
+    Nodes2 = rt:deploy_nodes(8, Conf, [riak_kv, riak_repl]),
+    [Nodes1, Nodes2].
+
+%%    Leaders = [((L*NumberOfNodesPerCluster)-5)+1 || L <- lists:seq(1,NumberOfClusters)],
+%%    [lists:sublist(Nodes, X, NumberOfNodesPerCluster) || X <- Leaders].
 
 
-%%connect_clusters({LeaderA, ClusterAName}, {LeaderB, ClusterBName}) ->
-%%    {ok, {_IP, Port}} = rpc:call(LeaderB, application, get_env, [riak_core, cluster_mgr]),
-%%    lager:info("connect cluster ~p:~p to ~p on port ~p", [ClusterAName, LeaderA, ClusterBName, Port]),
+%%connect_clusters(LeaderA, LeaderB) ->
+%%    {ok, {_IP, Port}} = rpc:call(LeaderB, application, get_env,
+%%        [riak_core, cluster_mgr]),
+%%    lager:info("connect cluster A:~p to B on port ~p", [LeaderA, Port]),
 %%    repl_util:connect_cluster(LeaderA, "127.0.0.1", Port),
-%%    ?assertEqual(ok, repl_util:wait_for_connection(LeaderA, ClusterBName)).
+%%    ?assertEqual(ok, repl_util:wait_for_connection(LeaderA, "B")).
+%%
+%%enable_rt(LeaderA, ANodes) ->
+%%    repl_util:enable_realtime(LeaderA, "B"),
+%%    rt:wait_until_ring_converged(ANodes),
+%%
+%%    repl_util:start_realtime(LeaderA, "B"),
+%%    rt:wait_until_ring_converged(ANodes).
+%%
+%%verify_rt(LeaderA, LeaderB) ->
+%%    TestHash =  list_to_binary([io_lib:format("~2.16.0b", [X]) ||
+%%        <<X>> <= erlang:md5(term_to_binary(os:timestamp()))]),
+%%    TestBucket = <<TestHash/binary, "-rt_test_a">>,
+%%    First = 101,
+%%    Last = 200,
+%%
+%%    %% Write some objects to the source cluster (A),
+%%    rt:log_to_nodes([LeaderA], "write objects (verify_rt)"),
+%%    lager:info("Writing ~p keys to ~p, which should RT repl to ~p",
+%%        [Last-First+1, LeaderA, LeaderB]),
+%%    ?assertEqual([], repl_util:do_write(LeaderA, First, Last, TestBucket, 2)),
+%%
+%%    %% verify data is replicated to B
+%%    rt:log_to_nodes([LeaderA], "read objects (verify_rt)"),
+%%    lager:info("Reading ~p keys written from ~p", [Last-First+1, LeaderB]),
+%%    ?assertEqual(0, repl_util:wait_for_reads(LeaderB, First, Last, TestBucket, 2)).
+
+
+
+%% ================================================================================================================== %%
+%%                                        Helper Functions                                                            %%
+%% ================================================================================================================== %%
